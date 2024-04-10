4a693c77496edb82df1ff51415661c7375d29ec3
cbc02009 <39525900+cbc02009@users.noreply.github.com>
2024-04-10 12:25:40 -0400

    don't remote build for shinobu

--- deploy.nix
+++ deploy.nix
@@ -23,7 +23,7 @@ let
 in
 {
   deploy.nodes = {
-    shinobu = deployConfig "shinobu" "x86_64-linux" {sshUser = "cbc02009"; remoteBuild = true;};
+    shinobu = deployConfig "shinobu" "x86_64-linux";
     yuzu = deployConfig "yuzu" "x86_64-linux" {sshUser = "cbc02009"; remoteBuild = true;};
   };
   checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
